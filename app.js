c0f2973c391de4857fc0b8a6da750f8728415e93
joshigwe777 <joshigwe777@gmail.com>
2021-08-09 14:56:05 -0400

    js functions added

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // global variables
 
 let employees = [];
-const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US`;
-const gridContainer = document.querySelector('.grid-container');
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
+email, location, phone, dob &noinfo &nat=US`;
+const gridContainer = document.querySelector(".container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
+
+
+        
 
 //fetch data from API
 
@@ -26,7 +30,7 @@ fetch(urlAPI)
         employees.forEach((employee, index) => {
             let name = employee.name;
             let email = employee.emil;
-            let city = emplpoyee.location.city;
+            let city = employee.location.city;
             let picture = employee.picutre;
 
             employee.employeeHTML += `
@@ -43,7 +47,41 @@ fetch(urlAPI)
         
         });
     }
-    // finish this function
-    function displayModal(index) {
 
+
+    function displayModal(index) { 
+        let { name, dob, phone, email, location: { city, street, state, postcode}, picture } = employees[index];
+
+        let date = new Date(dob.date);
+
+        const modalHTML = `
+        <img class="avatar" src="${picture.large}" />
+        <div class="text-container">
+            <h2 class="name">${name.first} ${name.last}</h2>
+            <p class="email">${email}</p>
+            <p class="address">${city}</p>
+            <hr />
+            <p>${phone}</p>
+            <p class="address">${street}, ${state} ${postcode}</p>
+            <p>Birthday: 
+            ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+        </div>
+        `;
+    
+        overlay.classList.remove("hidden");
+        modalContainer.innerHTML = modalHTML;
     }
+
+    // gridContainer.addEventListener('click', e => {
+        // if (e.target !== gridContainer) {
+            
+            // const card = e.target.closest(".card");
+            // const index = card.getAttribute('data-index');
+
+            // displayModal(index);
+        // }
+    // });
+
+    modalClose.addEventListener('click', () => {
+        overlay.classList.add("hidden");
+    });
